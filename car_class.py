7e47a20db4db052ae2027455c689d7e90ebbcebb
Bryoo <bryantskisseu@gmail.com>
2017-05-09 14:41:33 +0300

    Hacked car door tests and dedugged init constructor. Ran 11 tests, failed 2

--- car_class.py
+++ car_class.py
@@ -1,7 +1,7 @@
 class Car(object):
-    def __init__(self, speed=0, name='General', model='GM', vehicle_type=None):
+    def __init__(self, name='General', model='GM', vehicle_type=None):
         self.name = name
-        self.speed = speed
+        self.speed = 0
         self.model = model
         self.vehicle_type = vehicle_type
 
